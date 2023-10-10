70b0456651bfb9633494b8e41a09798379689172
Christian Jacobsen <crja@abgroup.dk>
2023-10-10 10:24:29 +0200

    cloud platform setup complete

--- scope-name/hello-world-app/hello-world-app.app-root.tsx
+++ scope-name/hello-world-app/hello-world-app.app-root.tsx
@@ -1,7 +1,8 @@
-import ReactDOM from 'react-dom/client';
-import { HelloWorld } from '@org/scope-name.ui.hello-world';
+import ReactDOM from "react-dom/client";
+import { HelloWorld } from "@bit-poc/crja-scope.ui.hello-world";
 
 export const HelloWorldApp = () => <HelloWorld />;
 
 const root = document!.getElementById('root');
 ReactDOM.createRoot(root as HTMLElement).render(<HelloWorldApp />);
+
